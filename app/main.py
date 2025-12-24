13a4dcb3938353dddc1bf4ca0bf4ea51628ea30b
Vic <icv0119@gmail.com>
2025-12-24 20:35:08 +0800

    new1

--- app/main.py
+++ app/main.py
@@ -2,9 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
 
 @app.get("/version")
 def version():
@@ -12,4 +9,4 @@ def version():
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World - Deployment Success"}
+    return {"message": "Deployment Success"}
